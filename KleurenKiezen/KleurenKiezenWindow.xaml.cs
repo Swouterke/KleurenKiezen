c6aa2c0a7c3f90744a7f82b40c9ed687096bc37d
Swouterke <wvercruysse45@gmail.com>
2018-04-18 15:43:26 +0200

    AddedClass Kleur AddedEventHandler buttonKleur

--- KleurenKiezen/KleurenKiezenWindow.xaml.cs
+++ KleurenKiezen/KleurenKiezenWindow.xaml.cs
@@ -14,7 +14,7 @@ using System.Windows.Shapes;
 using System.Reflection;
 
 
-namespace WpfCursus
+namespace KleurenKiezen
 {
     /// <summary>
     /// Interaction logic for KleurenKiezenWindow.xaml
@@ -24,6 +24,48 @@ namespace WpfCursus
         public KleurenKiezenWindow()
         {
             InitializeComponent();
+            foreach (PropertyInfo info in typeof(Colors).GetProperties())
+            {
+                BrushConverter bc = new BrushConverter();
+                SolidColorBrush deKleur = (SolidColorBrush)bc.ConvertFromString(info.Name);
+                Kleur kleurke = new Kleur();
+                kleurke.Borstel = deKleur;
+                kleurke.Naam = info.Name;
+                kleurke.Hex = deKleur.ToString();
+                kleurke.Rood = deKleur.Color.R;
+                kleurke.Groen = deKleur.Color.G;
+                kleurke.Blauw = deKleur.Color.B;
+                comboBoxKleuren.Items.Add(kleurke);
+                if (kleurke.Naam == "Black")
+                    comboBoxKleuren.SelectedItem = kleurke;
+            }
+        }
+
+        private void buttonKleur_Click(object sender, RoutedEventArgs e)
+        {
+            if (radioVoorgrond.IsChecked == true)
+            {
+                rechthoek.Fill = new SolidColorBrush(Color.FromRgb(
+                    Convert.ToByte(labelRed.Content.ToString()),
+                    Convert.ToByte(labelGreen.Content.ToString()),
+                    Convert.ToByte(labelBlue.Content.ToString())));
+                comboBoxKleuren.SelectedIndex = -1;
+                foreach (Kleur kleurnaam in comboBoxKleuren.Items)
+                {
+                    if (rechthoek.Fill.ToString() == kleurnaam.Hex)
+                    {
+                        comboBoxKleuren.SelectedItem = kleurnaam;
+                    }
+                }
+            }
+            if ((radioAchtergrond.IsChecked == true) && (comboBoxKleuren.SelectedIndex >= 0))
+            {
+                Kleur gekozenKleur = (Kleur)comboBoxKleuren.SelectedItem;
+                panelVoorbeeld.Background = gekozenKleur.Borstel;
+                sliderRed.Value = gekozenKleur.Rood;
+                sliderGreen.Value = gekozenKleur.Groen;
+                sliderBlue.Value = gekozenKleur.Blauw;
+            }
         }
     }
 }
